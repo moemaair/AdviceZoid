0bd067b07abd4ee2f5472619808747a01d8cb418
MohamedIbrahim <rageali12@gmail.com>
2022-11-18 21:28:58 +0300

    add Info Icon on Favorite Screen's Appbar

--- app/src/main/java/com/android/advicezoid/Favorite.kt
+++ app/src/main/java/com/android/advicezoid/Favorite.kt
@@ -2,24 +2,27 @@ package com.android.advicezoid
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.android.advicezoid.ui.theme.AdvicezoidTheme
 
 class Favorite : ComponentActivity() {
@@ -37,13 +40,24 @@ class Favorite : ComponentActivity() {
 fun FvComposable() {
     val context = LocalContext.current
     Scaffold(
-
         topBar = {
-            TopAppBar(modifier = Modifier.background(Color.Red)) {
-                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "", Modifier.clickable(onClick = {context.startActivity(
-                    Intent(context, MainActivity::class.java)
-                )}),)
-                Text(text = "Favorites Advice", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            TopAppBar( backgroundColor = Color.Transparent, elevation = 0.dp) {
+
+                Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack, contentDescription = "",
+                        Modifier.clickable(onClick = {
+                            context.startActivity(
+                                Intent(context, MainActivity::class.java)
+                            )
+                        }),
+                    )
+                    Text(
+                        text = "Favorites Advice",
+                        textAlign = TextAlign.Center
+                    )
+                    Icon(imageVector = Icons.Default.Info, contentDescription = "", modifier = Modifier)
+                }
             }
         }
     ){}
