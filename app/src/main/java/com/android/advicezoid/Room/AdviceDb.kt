8dbb7bc13b2c940abb7efa1125648ccd73b83da8
MohamedIbrahim <rageali12@gmail.com>
2022-12-18 12:50:00 +0300

    Add Room database class

---
+++ app/src/main/java/com/android/advicezoid/Room/AdviceDb.kt
@@ -0,0 +1,12 @@
+package com.android.advicezoid.Room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.android.advicezoid.model.Slip
+
+@Database(
+    entities = [Slip::class],
+    version = 1,
+    exportSchema = false)
+
+abstract class AdviceDb: RoomDatabase(){}
